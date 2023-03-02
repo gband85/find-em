30b0fbd91392fad3c3930ffb220ebfbffbb3a3e5
gband85 <garrett@gband.net>
2023-03-01 20:25:29 -0600

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
 import { useState } from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Root from './components/Root';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import PuzzleList from './components/PuzzleList';
+import Puzzle from './components/Puzzle';
+import { getPuzzle, getPuzzles } from './components/puzzles';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+  
+{
+      element:<Root />, 
+      errorElement:<ErrorPage/>,
+      children: [
+{
+path: "/",
+element: <Home/>
+},
+{
+  path: "puzzles",
+  children: [
+    {
+element: <PuzzleList/>,
+loader: async()=>getPuzzles(),
+    },
+    {
+      path: ":puzzleTitle",
+      element: <Puzzle/>,
+loader: async ({params})=>getPuzzle(params.puzzleTitle)
+    }
+  ]
+}
+      ]
+    }
+])
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+return (
+<RouterProvider router={router} />
+)
 }
 
 export default App
