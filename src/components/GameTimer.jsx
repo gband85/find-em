6d4df3070a536e328f3e13a11898987a32a3f717
gband85 <garrett@gband.net>
2023-03-02 15:53:51 -0600

    fill out gameTimer

--- src/components/GameTimer.jsx
+++ src/components/GameTimer.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const GameTimer = () => {
+const GameTimer = (props) => {
+    const [timeElapsed,setTimeElapsed] = useState(0)
+    const [minutes,setMinutes]=useState(Math.floor(0))
+    const [seconds,setSeconds]=useState(0)
+
+    
+   
+    
+    useEffect(()=>{
+      let timer=()=>
+      {
+       
+        setTimeElapsed(timeElapsed+1000)
+        props.setTimeElapsed(timeElapsed)
+         setMinutes(Math.floor(timeElapsed / 1000 / 60))
+             setSeconds(timeElapsed/1000%60)
+     
+   }
+      let  tick;
+      if (props.gameOver) {
+       
+           clearInterval(tick)
+} 
+      
+else {
+   tick  = setInterval(timer,1000)
+}
+return ()=>  clearInterval(tick)
+
+
+    })
+  
   return (
-    <div>GameTimer</div>
+  <div id="clock">
+  
+  
+                  <h1 id="time-left">{(minutes<10 ? "0" + minutes : minutes)+":"+(seconds<10 ? "0"+seconds:seconds)}</h1>
+                 
+      
+             
+          </div>
   )
 }
 
