73e420b5a91f5b15e283894da63bdbfea504d51c
gband85 <garrett@gband.net>
2023-03-02 13:07:05 -0600

    add html and loader

--- src/components/PuzzleList.jsx
+++ src/components/PuzzleList.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useLoaderData } from 'react-router-dom';
+import { getPuzzles } from './puzzles';
+
+export async function puzzleListLoader() {
+  //await resetPuzzles();
+  const puzzles = await getPuzzles();
+  console.log({ puzzles });
+  return { puzzles };
+}
 
 const PuzzleList = () => {
-  return (
-    <div>PuzzleList</div>
-  )
+   // const {puzzles} = useContext(store).state;
+   const { puzzles } = useLoaderData();
+   useEffect(()=>{
+    // console.log(globalState);
+   })
+ 
+   return (
+     <div className="puzzles">
+       {puzzles.map((puzzle) => {
+         return (
+           <>
+             <Link
+               className="puzzle-link"
+               to={`/puzzles/${puzzle.title}`}
+               
+             >
+               <div style={{ border: "1px solid red" }}>
+                 <h1>{puzzle.title}</h1>
+                 <img src={puzzle.source} />
+               </div>
+             </Link>
+           </>
+         );
+       })}
+     </div>
+   );
 }
 
 export default PuzzleList
