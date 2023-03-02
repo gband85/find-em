e8978266b28536beae7e8ddaae906af0589c9eab
gband85 <garrett@gband.net>
2023-03-02 13:12:05 -0600

    add puzzle layout

--- src/components/Puzzle.jsx
+++ src/components/Puzzle.jsx
@@ -8,7 +8,76 @@ export async function puzzleLoader({ params }) {
 
 const Puzzle = () => {
   return (
-    <div>Puzzle</div>
+    <>
+      {" "}
+      <div className="puzzle">
+      gameOver ?  <Overlay overlayDisplay={overlayDisplay} /> : null
+
+        
+        <div className="puzzle-header">
+          <h1>{puzzle.title}</h1>
+          <button className="reset" onClick={handleClick} type="button">Reset</button>
+          <GameTimer gameOver={gameOver} setTimeElapsed={setTimeElapsed}/>
+        </div>
+        <div
+          className="game-over"
+          style={gameOver ? { display: "flex" } : { display: "none" }}
+        >
+          <h1>Congratulations! You finished in {timeElapsed}</h1>
+          <button className="reset"
+            onClick={handleClick}
+            type="button"
+          >
+            Try Again
+          </button>
+        </div>
+        <div className="puzzle-body">
+          <img
+            src={puzzle.source}
+            className="image"
+            onClick={handleClick}
+          />
+
+          {puzzle.chars.map((char) => {
+            return (
+              <CharBox
+                key={char.name}
+                char={char}/>
+            );
+          })}
+
+          <div
+            className="find-list"
+            style={{
+              left: `${boxStyle.left}`,
+              top: `${boxStyle.top}`,
+              display: boxStyle.display,
+            }}
+          >
+            <ul>
+              {puzzle.chars.map((char) => {
+                let disabled;
+                if (char.found) {
+                  disabled = true;
+                }
+                return (
+                  <li className="char" key={char.name}>
+                    <button
+                      data-name={char.name}
+                      type="submit"
+                      disabled={disabled}
+                      onClick={handleClick}
+                    >
+                      {char.name}
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
