c1493e8aa10588e4b2fc84c8e24ee220008da5ec
gband85 <garrett@gband.net>
2023-03-12 20:53:49 -0500

    add score input

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,8 +1,11 @@
+
 const GameOver=({timeDisplay,handleClick}) =>{
     return <div
       className="game-over">
       <h1>Congratulations! You finished in {timeDisplay}</h1>
-      <button className="reset"
+      <input type="text" placeholder='Name' onChange={(e)=>setScore({Name: e.target.value,Time:"00:11:32"})}/>
+    <button type='submit' onClick={()=>addScore(score)}>Add Score</button>
+      <button id="reset" className="button is-primary"
         onClick={handleClick}
         type="button"
       >
