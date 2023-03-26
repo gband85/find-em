b3fc7d6fbec94e5ec278cf2da9b6e0a9777101c5
gband85 <garrett@gband.net>
2023-03-26 18:04:09 -0500

    add a form

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -2,9 +2,11 @@
 const GameOver=({timeDisplay,handleClick}) =>{
     return <div
       className="game-over">
-      <h1>Congratulations! You finished in {timeDisplay}</h1>
-      <input type="text" placeholder='Name' onChange={(e)=>setScore({Name: e.target.value,Time:"00:11:32"})}/>
-    <button type='submit' onClick={()=>addScore(score)}>Add Score</button>
+      <form onSubmit={handleSubmit} className="is-flex">
+
+      <input required placeholder='Name' name="Name" value={score.Name} onChange={handleChange}/>
+    <button type="submit" className="button is-primary" >Add Score</button>
+    </form>
       <button id="reset" className="button is-primary"
         onClick={handleClick}
         type="button"
