9632b9d52c8ae0d06a1a1819b3c6925f12b7d970
gband85 <garrett@gband.net>
2023-03-26 18:00:07 -0500

    add useInterval

--- src/components/GameTimer.jsx
+++ src/components/GameTimer.jsx
@@ -1,47 +1,38 @@
 import React, { useEffect, useState } from 'react'
+import { useRouteLoaderData } from 'react-router-dom'
+import { useInterval } from './useInterval'
 
 const GameTimer = (props) => {
     const [timeElapsed,setTimeElapsed] = useState(0)
     const [hours,setHours]=useState(0)
     const [minutes,setMinutes]=useState(0)
     const [seconds,setSeconds]=useState(0)
+const [timeDisplay, setTimeDisplay] = useState("00:00:00")
 
     const time_left=(
-      <h1 id="time-left">{(hours<10 ? "0" + hours : hours)+ ":" +(minutes<10 ? "0" + minutes : minutes)+":"+(seconds<10 ? "0"+seconds:seconds)}</h1>)
-   
-    
-    useEffect(()=>{
-      let timer=()=>
+      (hours<10 ? "0" + hours : hours)+ ":" +(minutes<10 ? "0" + minutes : minutes)+":"+(seconds<10 ? "0"+seconds:seconds))
+      useInterval(()=>
       {
        
         setTimeElapsed(timeElapsed+1000)
         setHours(Math.floor(timeElapsed/1000/60/60%24))
          setMinutes(Math.floor(timeElapsed / 1000 / 60 % 60))
              setSeconds(timeElapsed/1000%60)
-     props.setTimeDisplay(time_left)
-   }
+             props.setTimeDisplay(time_left)
+   },props.isRunning?1000:null)
+    
+
       let  tick;
-      if (props.gameOver) {
-       
-           clearInterval(tick)
-} 
-      
-else {
-   tick  = setInterval(timer,1000)
-}
-return ()=>  clearInterval(tick)
-
-
-    })
+      if (!props.gameOver) {}
   
   return (
-  <div id="clock" className='navbar-item'>
-  
-  {time_left}
+
+  <h1 className='is-size-4-mobile is-size-4-touch is-size-3-desktop' id="time-left">
+  {props.timeDisplay}</h1>
                  
       
              
-          </div>
+
   )
 }
 
