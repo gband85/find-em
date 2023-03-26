da61cb1ad2c78eb702ca6aa54c0aa49c2ca8e734
gband85 <garrett@gband.net>
2023-03-26 17:59:01 -0500

    tweak navbar

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -18,8 +18,8 @@ setActive(!active)
   return (
     
     <>
-      <nav className="navbar is-info">
-        <div className="navbar-brand">
+      <nav className="navbar is-info"  role="navigation" aria-label="main navigation">
+        <div className="navbar-brand is-size-3">
           <Link to="/" className="navbar-item" id='brand'>
             Find 'Em
           </Link>
@@ -31,16 +31,14 @@ setActive(!active)
         </div>
 
         <div className={active ? "navbar-menu is-active" : "navbar-menu"}>
-          <div className="navbar-start"></div>
 
           <div className="navbar-end">
-            {data ? (
+      {data ? (
               <>
-                {" "}
                 
                   
                     {data.chars.map((char) => {
-                      return <div className="navbar-item">{char.name}</div>;
+                      return <a className="navbar-item">{char.name}</a>;
                     })}
                   
                 
@@ -49,14 +47,14 @@ setActive(!active)
             ) : (
               <>
                 <NavLink to="/info" className={({isActive,isPending})=>
-                        isActive ? "navbar-item is-active":"navbar-item"
+                        isActive ? "navbar-item is-active is-size-4":"navbar-item is-size-4"
                 }>
                   Info
                 </NavLink>
 <NavLink to="/" className={({isActive,isPending})=>                
-                isActive ? "navbar-item is-active":"navbar-item"
+                isActive ? "navbar-item is-active is-size-4":"navbar-item is-size-4"
             }>Puzzles</NavLink>
-                <NavLink to="/leaderboard/PS3" className={({isActive,isPending})=> isActive ? "navbar-item is-active":"navbar-item"
+                <NavLink to="/leaderboard/PS3" className={({isActive,isPending})=> isActive ? "navbar-item is-active is-size-4":"navbar-item is-size-4"
                     }>
                   Leaderboard
                 </NavLink>
