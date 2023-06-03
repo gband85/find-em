4b746b97562c99b4cae8f334f7268423203036c5
Garrett Anderson <gband85@mailfence.com>
2023-06-03 17:53:14 -0500

    add Leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { collection, addDoc,getDocs } from "firebase/firestore";
-import {db} from './firebase-config';
-      import { useLoaderData, useRouteLoaderData } from 'react-router-dom';
-import { getScores } from './firebase';
+//import { collection, addDoc,getDocs } from "firebase/firestore";
+//import {db} from './firebase-config';
+import { useLoaderData, useRouteLoaderData } from 'react-router-dom';
+import { getScores } from './firebasefns';
 // Initialize Firebase
 function Leaderboard() {
 const {scores,puzzle}=useLoaderData()
