92702314cbbe21e71c6033d7691557b6e942e8e6
Garrett Anderson <gband85@mailfence.com>
2023-07-15 14:27:18 -0500

    remove firebaseui

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -2,8 +2,6 @@ import { useEffect, useRef,useState } from "react";
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import * as firebaseui from 'firebaseui'
-import 'firebaseui/dist/firebaseui.css'
 import {db} from './firebase-config';
 import { useNavigate } from "react-router-dom";
 import { addScore } from "./firebasefns";
@@ -12,40 +10,13 @@ import SignInScreen from "./SignInScreen";
 
 const GameOver=(props) =>{
   const [googleUser,setGoogleUser]=useState({})
- // var ui = new firebaseui.auth.AuthUI(firebase.auth());
- const uiConfig = {
-  signInFlow: 'popup',
-  signInSuccessUrl: '/',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    //firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    //firebase.auth.PhoneAuthProvider.PROVIDER_ID
-  ],
 
-};
-
-
- useEffect(()=>{
-  const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
-  ui.start(document.querySelector("#firebaseui-auth-container"), uiConfig);
-})
 const ref=useRef()
   const navigate = useNavigate();
     const [score,setScore]=useState({
     Name: "",
     Time: props.timeDisplay
 })
-
-
-
-      // Initialize the FirebaseUI Widget using Firebase.
-//      const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
-      // The start method will wait until the DOM is loaded.
-  //    ui.start(document.querySelector("#firebaseui-auth-container"), uiConfig);
 
 const handleSubmit=(e)=>{
   console.log(score);
@@ -61,7 +32,6 @@ addScore({...score,Name:googleUser.displayName},props)
       <p>{googleUser.displayName}</p> 
       <SignInScreen setGoogleUser={setGoogleUser}/>
 <div id="loader">Loading...</div>
-{/* <button onClick={()=>ui.start(document.querySelector('#firebaseui-auth-container'), uiConfig)}>Sign in</button> */}
       <form onSubmit={handleSubmit} className="is-flex">
 
     <button type="submit" className="button is-primary" >Add Score</button>
