4e01713be954b97a479edffed51f081d5317f27a
Garrett Anderson <gband85@mailfence.com>
2023-07-24 13:49:01 -0500

    remove facebook login

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,29 +1,30 @@
-import { useEffect, useRef,useState } from "react";
-
+import { useEffect, useRef, useState } from "react";
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import {db} from './firebase-config';
+import { db } from './firebase-config';
 import { useNavigate } from "react-router-dom";
 import { addScore } from "./firebasefns";
 import '../css/GameOver.css'
 import SignInScreen from "./SignInScreen";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+import { GithubLoginButton, GoogleLoginButton } from "react-social-login-buttons";
 
-const GameOver=(props) =>{
-  const [googleUser,setGoogleUser]=useState({})
+const GameOver = (props) => {
+  const [userData, setUserData] = useState({})
 
-const ref=useRef()
+  const ref = useRef()
   const navigate = useNavigate();
-    const [score,setScore]=useState({
+  const [score, setScore] = useState({
     Name: "",
     Time: props.timeDisplay
 })
 
-const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
   console.log(score);
 e.preventDefault()
-addScore({...score,Name:googleUser.displayName},props)
-    //props.setGameOver(false)
-  //  props.reset()
+    addScore({ ...score, Name: userData.displayName }, props)
+    props.setGameOver(false)
+    props.reset()
     navigate(`/leaderboard/${props.title}`)
 }
     return (<div
@@ -34,7 +35,7 @@ addScore({...score,Name:googleUser.displayName},props)
 <div id="loader">Loading...</div>
       <form onSubmit={handleSubmit} className="is-flex">
 
-    <button type="submit" className="button is-primary" >Add Score</button>
+      {userData.displayName ? <button type="submit" className="button is-primary" >Add Score</button> : <button type="submit" disabled className="button is-primary" >Add Score</button>}
     </form>
       <button id="reset" className="button is-primary"
         onClick={props.handleClick}
@@ -42,6 +43,26 @@ addScore({...score,Name:googleUser.displayName},props)
       >
         Exit
       </button>
+    <GoogleLoginButton
+      onClick={async () => {
+        let user = await signInWithPopup(getAuth(), new GoogleAuthProvider())
+        if (user.user.displayName == null) {
+          setUserData({ ...user.user, displayName: "Anonymous Coward" })
+        }
+        else
+          setUserData(user.user)
+      }}
+    />
+    <GithubLoginButton
+      onClick={async () => {
+        let user = await signInWithPopup(getAuth(), new GithubAuthProvider())
+        if (user.user.displayName == null) {
+          setUserData({ ...user.user, displayName: "Anonymous Coward" })
+        }
+        else
+          setUserData(user.user)
+      }}
+    />
     </div>)
   }
   export default GameOver;
