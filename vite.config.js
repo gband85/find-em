bcd4822f3bc039499c7a5f5b293740acb3dbb912
Garrett Anderson <gband85@mailfence.com>
2023-07-25 11:41:48 -0500

    add base property

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/find-em/",
+  base: "/projects/find-em/",
   plugins: 
   [
     react(),
